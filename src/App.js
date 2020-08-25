8ec184c5f01009c61c6d9ad6dd092d19c200d93d
relMor <amanvarma.600847@gmail.com>
2020-08-25 14:13:52 +0530

    completed Row component and fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+
 import './App.css';
+import Row from './components/Row/Row';
+import requests from './requests'
 
 function App() {
   return (
     <div className="app">
       <h1>Netflix Clone</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
