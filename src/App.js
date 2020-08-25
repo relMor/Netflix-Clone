03c1af545ab72becc551eb3da50bfd67a0a06507
relMor <amanvarma.600847@gmail.com>
2020-08-25 15:06:24 +0530

    completed Banner component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import Row from './components/Row/Row';
-import requests from './requests'
+import "./App.css";
+import Row from "./components/Row/Row";
+import requests from "./requests";
+import Banner from "./components/Banner/Banner";
 
 function App() {
   return (
     <div className="app">
-      <h1>Netflix Clone</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      {/* Nav */}
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -5,7 +5,7 @@ import axios from "../../axios";
 
 const baseUrl = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -25,9 +25,11 @@ function Row({ title, fetchUrl }) {
       <div className="row__posters">
         {movies.map((movie) => (
           <img
-          key={movie.id}
-            className="row__poster"
-            src={`${baseUrl}${movie.poster_path}`}
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${baseUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
